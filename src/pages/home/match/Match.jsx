93914cf58236e6671250dbb39fec252262103237
moussa <mousamohamed260@gmail.com>
2023-01-03 04:46:51 +0200

    reservation price

--- src/pages/home/match/Match.jsx
+++ src/pages/home/match/Match.jsx
@@ -6,60 +6,94 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocation, faTicket } from '@fortawesome/free-solid-svg-icons'
 import { faEye } from '@fortawesome/free-regular-svg-icons'
 
+
+// function calculate(value) {
+//   var price = 0;
+//   var category = document.getElementById("Category").value;
+//   var tickets = document.getElementById("Tickets").value;
+//   var total = document.getElementById("total-price");
+//   if (category == 1) {
+//     price = 100;
+//   } else if (category == 2) {
+//     price = 200;
+//   } else if (category == 3) {
+//     price = 300;
+//   }
+//   var total = price * tickets;
+//   total.innerHTML = total;
+// }
 const Match = () => {
   return (
 
     <div className='match'>
       <Navbar />
-      <img className='matchImage' src="https://i.ibb.co/JQZhpL6/tunisiavsfrance.png"></img>
+      <img className='matchImage' src="https://i.imgur.com/H9nvdty.png"></img>
       <div className="matchContainer">
         <div className="matchWrapper">
           <h1 className="matchTitle"></h1>
 
           {/* <div className="matchImage">
           </div> */}
-          <div className="matchInfo">
-
-            <div className="Category">
-              <FontAwesomeIcon className="CategoryIcon" icon={faEye} />
-              <span> </span>
-              {/* <img src="https://i.ibb.co/NsrPvrL/qatarvssenegal.png"></img> */}
-              <label for="Category">Choose a Category:</label>
-              <select data-menu name="Category">
-                <option >1</option>
-                <option >2</option>
-                <option >3</option>
-              </select>
-            </div>
 
 
-            <div className="Tickets">
-              <FontAwesomeIcon className="CategoryIcon" icon={faTicket} />
-              <span> </span>
-              <label for="Category"> Number of tickets:</label>
-              <select data-menu name="Category">
-                <option onclick="PriceCalc(1)" >1</option>
-                <option onclick="PriceCalc(2)" >2</option>
-                <option onclick="PriceCalc(3)" >3</option>
-                <option onclick="PriceCalc(4)" >4</option>
-                <option onclick="PriceCalc(5)" >5</option>
-                <option onclick="PriceCalc(6)" >6</option>
-              </select>
-             
-              </div>
-              <div className="procceed">
-              <button> Proceed to checkout</button>
+          <div className="Category">
+            <FontAwesomeIcon className="CategoryIcon" icon={faEye} />
+            {/* <img src="https://i.ibb.co/NsrPvrL/qatarvssenegal.png"></img> */}
+            <label for="Category">Choose a Category:</label>
+            <input type="hidden" name="Category" value="Categoryno" />
+            <select onChange={() => calculate(this.value)} name="Category" id='Category' required>
+              <option value="" disabled selected >?</option>
+              <option value="1" >1</option>
+              <option value="2" >2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
 
-            </div>
-            
+
+          <div className="Tickets">
+            <FontAwesomeIcon className="CategoryIcon" icon={faTicket} />
+            <input type="hidden" name="Tickets" value="Ticketsno" />
+            <label for="Category"> Number of tickets:</label>
+            <select onChange={() => calculate(this.value)} data-menu name="Tickets" id="Tickets">
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
 
           </div>
+          <div className="procceed">
+            <button> Proceed to checkout</button>
+
+          </div>
+
+          <div className="Price">
+            <label for="Price"> Total Price:</label>
+            <div class="total-price" id="total-price">0</div>
+
+          </div>
+
+
+
         </div>
       </div>
     </div>
-    
+
   )
-  
+  function calculate() {
+    var price = 0;
+    var category = document.getElementByName("Category").value;
+    var tickets = document.getElementByName("Tickets").value;
+    var total = document.getElementByName("total-price");
+    if (category == 1) {
+      price = 100;
+    } else if (category == 2) {
+      price = 200;
+    } else if (category == 3) {
+      price = 300;
+    }
+    var total = price * tickets;
+    total.innerHTML = total;
+  }
+
 }
 
 export default Match
